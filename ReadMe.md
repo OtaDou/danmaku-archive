2060da0c7a282383a35dc6f4326f2afd92ec0fc3
KyleZhou <lueu@live.com>
2024-01-01 16:14:22 +0800

    update 2017-summer

---
+++ ReadMe.md
@@ -0,0 +1,7 @@
+# 2017-summer
+| NAME | EPISODE |
+| --- | --- |
+| ようこそ実力至上主義の教室へ | 12 |
+| 終物語2 | 7 |
+
+### Download [2017-summer.zip](https://github.com/OtaDou/danmaku-archive/archive/refs/heads/2017-summer.zip)
--- sites/niconico.spec.js
+++ sites/niconico.spec.js
@@ -19,20 +19,20 @@ test.beforeEach(async ({ page }) => {
   )
 })
 
-test("archive_folder_name1", async ({ page }, testInfo) => {
+test("ようこそ実力至上主義の教室へ", async ({ page }, testInfo) => {
   const config = {
     seriesName: testInfo.title,
-    homePage: "https://anime.nicovideo.jp/detail/XXXXX/index.html",
+    selector: `a.thumb_anchor.g-video-link`,
+    homePage: "https://ch.nicovideo.jp/you-zitsu",
   }
 
   await autoDownloadDanmaku(page, config)
 })
-
-test("archive_folder_name2", async ({ page }, testInfo) => {
+test("終物語2", async ({ page }, testInfo) => {
   const config = {
     seriesName: testInfo.title,
     selector: `a.thumb_anchor.g-video-link`,
-    homePage: "https://ch.nicovideo.jp/XXXXXXX",
+    homePage: "https://ch.nicovideo.jp/owarimonogatari2",
   }
 
   await autoDownloadDanmaku(page, config)
