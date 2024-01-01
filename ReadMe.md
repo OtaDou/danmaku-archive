a5b75c25954134779792ddfb1e614177f335322d
KyleZhou <lueu@live.com>
2024-01-02 00:12:56 +0800

    update 2015-autumn

---
+++ ReadMe.md
@@ -0,0 +1,6 @@
+# 2015-autumn
+| NAME | EPISODE |
+| --- | --- |
+| 終物語 | 12 |
+
+### Download [2015-autumn.zip](https://github.com/OtaDou/danmaku-archive/archive/refs/heads/2015-autumn.zip)
--- sites/niconico.spec.js
+++ sites/niconico.spec.js
@@ -19,20 +19,11 @@ test.beforeEach(async ({ page }) => {
   )
 })
 
-test("archive_folder_name1", async ({ page }, testInfo) => {
-  const config = {
-    seriesName: testInfo.title,
-    homePage: "https://anime.nicovideo.jp/detail/XXXXX/index.html",
-  }
-
-  await autoDownloadDanmaku(page, config)
-})
-
-test("archive_folder_name2", async ({ page }, testInfo) => {
+test("終物語", async ({ page }, testInfo) => {
   const config = {
     seriesName: testInfo.title,
     selector: `a.thumb_anchor.g-video-link`,
-    homePage: "https://ch.nicovideo.jp/XXXXXXX",
+    homePage: "https://ch.nicovideo.jp/owarimonogatari",
   }
 
   await autoDownloadDanmaku(page, config)
