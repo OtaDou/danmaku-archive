82dcdcb3a337e379afd799a4c5def4bdb18cb0a3
KyleZhou <lueu@live.com>
2024-01-03 22:43:07 +0800

    update template

---
+++ ReadMe.md
@@ -0,0 +1,6 @@
+# template
+| NAME | EPISODE |
+| --- | --- |
+| 偽物語 | 11 |
+
+### Download [template.zip](https://github.com/OtaDou/danmaku-archive/archive/refs/heads/template.zip)
--- sites/niconico.spec.js
+++ sites/niconico.spec.js
@@ -19,20 +19,11 @@ test.beforeEach(async ({ page }) => {
   )
 })
 
-test("archive_folder_name1", async ({ page }, testInfo) => {
-  const config = {
-    seriesName: testInfo.title,
-    homePage: "https://anime.nicovideo.jp/detail/XXXXX/index.html",
-  }
-
-  await autoDownloadDanmaku(page, config)
-})
-
-test("archive_folder_name2", async ({ page }, testInfo) => {
+test("偽物語", async ({ page }, testInfo) => {
   const config = {
     seriesName: testInfo.title,
     selector: `a.thumb_anchor.g-video-link`,
-    homePage: "https://ch.nicovideo.jp/XXXXXXX",
+    homePage: "https://ch.nicovideo.jp/nisemonogatari-anime",
   }
 
   await autoDownloadDanmaku(page, config)
