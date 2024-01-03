6ee5a78a667e3c56cc2b878a7b88c974c4408ce8
KyleZhou <lueu@live.com>
2024-01-03 22:09:46 +0800

    update 2012-autumn

---
+++ ReadMe.md
@@ -0,0 +1,6 @@
+# 2012-autumn
+| NAME | EPISODE |
+| --- | --- |
+| 猫物語(黒) | 4 |
+
+### Download [2012-autumn.zip](https://github.com/OtaDou/danmaku-archive/archive/refs/heads/2012-autumn.zip)
--- sites/niconico.spec.js
+++ sites/niconico.spec.js
@@ -19,20 +19,12 @@ test.beforeEach(async ({ page }) => {
   )
 })
 
-test("archive_folder_name1", async ({ page }, testInfo) => {
-  const config = {
-    seriesName: testInfo.title,
-    homePage: "https://anime.nicovideo.jp/detail/XXXXX/index.html",
-  }
 
-  await autoDownloadDanmaku(page, config)
-})
-
-test("archive_folder_name2", async ({ page }, testInfo) => {
+test("猫物語(黒)", async ({ page }, testInfo) => {
   const config = {
     seriesName: testInfo.title,
     selector: `a.thumb_anchor.g-video-link`,
-    homePage: "https://ch.nicovideo.jp/XXXXXXX",
+    homePage: "https://ch.nicovideo.jp/nekomonogatari",
   }
 
   await autoDownloadDanmaku(page, config)
