ee087b3c4d2d32e0e75edeb9d4f594698b1e960e
KyleZhou <lueu@live.com>
2025-04-02 14:30:01 +0800

    skip 特番

--- sites/niconico.spec.js
+++ sites/niconico.spec.js
@@ -55,6 +55,10 @@ async function autoDownloadDanmaku(page, config) {
   for await (const link of newLinks) {
     await page.goto(link, { waitUntil: "domcontentloaded" })
     let title = (await page.title()).replace(" - ニコニコ動画", "").trim()
+    if (/特別番組|総集編|直前特番/.test(title)) {
+      console.log(`skip... ${title}`)
+      continue
+    }
     title = reservedCharReplace(title)
     await Promise.all([
       page.reload({ waitUntil: "domcontentloaded" }),
