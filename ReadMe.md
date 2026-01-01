9e96192b54b38f9714926695317c752fd1a6c0f1
KyleZhou <lueu@live.com>
2026-01-01 16:46:59 +0800

    2022 winter

---
+++ ReadMe.md
@@ -0,0 +1,6 @@
+# 2022-winter
+| NAME | EPISODE |
+| --- | --- |
+| 怪人開発部の黒井津さん | 12 |
+
+### Download [2022-winter.zip](https://github.com/OtaDou/danmaku-archive/archive/refs/heads/2022-winter.zip)
--- sources/niconico.spec.js
+++ sources/niconico.spec.js
@@ -16,8 +16,7 @@ const danmakuConfig = {
 
 // --- 任务配置表 (新增番剧只需在此添加一行) ---
 const TASKS = [
-  { name: "archive_folder_name1", url: "https://anime.nicovideo.jp/detail/XXXXX/index.html" },
-  { name: "archive_folder_name2", url: "https://ch.nicovideo.jp/XXXXXXX", selector: VIDEO_SELECTOR_ALT },
+  { name: "怪人開発部の黒井津さん", url: "https://anime.nicovideo.jp/detail/kuroitsusan-anime/index.html" },
 ]
 
 test.beforeEach(async ({ page }) => {
